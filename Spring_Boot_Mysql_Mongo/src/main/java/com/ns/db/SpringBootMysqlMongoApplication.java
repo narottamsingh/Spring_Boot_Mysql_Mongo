362d02695627bd5d070ec0d710ae776cbe542b68
Narottam Singh <narottam.singh@50hertz.in>
2018-05-16 11:24:33 +0530

    Init

---
+++ Spring_Boot_Mysql_Mongo/src/main/java/com/ns/db/SpringBootMysqlMongoApplication.java
@@ -0,0 +1,12 @@
+package com.ns.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlMongoApplication.class, args);
+	}
+}
